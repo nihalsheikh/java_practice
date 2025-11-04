b8bb28956b562fd116e20e6cc013db003042ccbc
nihal <nihalsheikh585@gmail.com>
2025-11-04 22:58:39 +0530

    Module: variables and datatypes

---
+++ 01_variables-and-datatypes/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+	public static void main(String[] args) {
+		// Declaring a variable
+		int a = 10;
+		int b = 5;
+		System.out.println(a);
+		System.out.println(b);
+
+		String name = "John Doe";
+		System.out.println(name);
+
+		// Changing value of an existing variable
+		a = 100;
+		System.out.println(a);
+	}
+}
