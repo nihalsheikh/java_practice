5ed1eb56ff56d15adc767f24e87659020984cb57
nihal <nihalsheikh585@gmail.com>
2025-11-05 11:01:01 +0530

    Datatypes

---
+++ 01_variables-and-datatypes/Datatypes.java
@@ -0,0 +1,35 @@
+public class Datatypes {
+	public static void main(String[] args) {
+		// Byte ( SIZE - 1 byte )
+		byte b = 8;
+		System.out.println(b);
+
+		// Short ( SIZE - 2 byte )
+		short s = 3000;
+		System.out.println(s);
+
+		// Integer ( SIZE - 4 byte )
+		int num = 100;
+		System.out.println(num);
+
+		// Long - for very long integer values ( SIZE - 8 byte )
+		long longNum = 100000000L;
+		System.out.println(longNum);
+
+		// Float - decimal nums ( SIZE - 4 byte )
+		float fl = 1.123f;
+		System.out.println(fl);
+
+		// Double - for very long float values ( SIZE - 8 byte )
+		double d = 3.1415d;
+		System.out.println(d);
+
+		// Characters ( SIZE - 2 byte )
+		char ch = 'c';
+		System.out.println(ch);
+
+		// Boolean ( SIZE - 1 byte )
+		boolean bool = true;
+		System.out.println(bool);
+	}
+}
